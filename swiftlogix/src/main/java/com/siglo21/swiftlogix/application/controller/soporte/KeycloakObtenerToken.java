65f336f65c0db86e8c45a4c0b27a5746757ad9c2
Facundo Luna <89422@sistemas.frc.utn.edu.ar>
2024-01-05 12:23:21 -0300

    peticion de token con usuario y contrasenia

--- swiftlogix/src/main/java/com/siglo21/swiftlogix/application/controller/soporte/KeycloakObtenerToken.java
+++ swiftlogix/src/main/java/com/siglo21/swiftlogix/application/controller/soporte/KeycloakObtenerToken.java
@@ -1,32 +1,28 @@
 package com.siglo21.swiftlogix.application.controller.soporte;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.Map;
-
-@CrossOrigin
 @RestController
 @RequestMapping("/api/v1/keycloak")
 public class KeycloakObtenerToken {
 
     private static final String TOKEN_ENDPOINT = "http://localhost:8180/auth/realms/siglo21/protocol/openid-connect/token";
 
-    ///api/v1/keycloak/obtener-token
-
-    @PostMapping("/obtener-token")
+    @CrossOrigin
+    @GetMapping("/obtener-token")
     @ResponseBody
-    public String obtenerToken(@RequestBody Map<String, String> requestBody) {
+    public String obtenerToken(@RequestParam String username, @RequestParam String password) {
         // Configurar el encabezado de la solicitud
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
@@ -35,8 +31,8 @@ public class KeycloakObtenerToken {
         MultiValueMap<String, String> map= new LinkedMultiValueMap<>();
         map.add("client_id", "siglo21-client-api-rest");
         map.add("grant_type", "password");
-        map.add("username", "alejo.dev");
-        map.add("password", "andes2020");
+        map.add("username", username);
+        map.add("password", password);
         map.add("client_secret", "DuXMmJE5ClpWLFZvYW0epQydZowD9msw");
 
         // Realizar la solicitud POST
@@ -45,5 +41,4 @@ public class KeycloakObtenerToken {
         String token = ((responseKeycloak) response.getBody()).getAccess_token();
         return token;
     }
-
 }
