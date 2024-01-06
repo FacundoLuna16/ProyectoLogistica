0006180155edc352b20a6c14a29491c521c762db
Alejo <alejotomasherrera@gmail.com>
2024-01-05 23:19:14 -0300

    fuera el cerrar sesion

--- frontEnd/src/layouts/dashboard/account-popover.jsx
+++ frontEnd/src/layouts/dashboard/account-popover.jsx
@@ -46,7 +46,6 @@ export const AccountPopover = (props) => {
           },
         }}
       >
-        <MenuItem onClick={handleSignOut}>Cerrar sesion</MenuItem>
       </MenuList>
     </Popover>
   );
